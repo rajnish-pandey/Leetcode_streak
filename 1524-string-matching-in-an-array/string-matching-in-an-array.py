fb7dbd2cfd2e1461162244c9d162792d30975473
Rajnish Pandey <62322903+rajnish-pandey@users.noreply.github.com>
2025-01-07 23:20:22 +0530

    Time: 3 ms (79.46%) | Memory: 12.3 MB (45.54%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -3,18 +3,9 @@ class Solution(object):
         answer = []
         for i in words:
             for j in words:
-                if i in j and i != j:
+                if (i in j) and (i != j):
                     answer.append(i)
                     break
         return answer
-
-        # answer = []
-        # n = len(words)
-        # for i in range(n):
-        #     for j in range(n):
-        #         if i != j and words[i] in words[j]:
-        #             answer.append(words[i])
-        #             break
-        # return answer
             
         
