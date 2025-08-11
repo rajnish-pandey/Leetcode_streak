fdcaae2a888111dadcba7916de0a803c0fa00bc3
Rajnish Pandey <62322903+rajnish-pandey@users.noreply.github.com>
2025-08-11 18:24:07 +0530

    Time: 12 ms (32.36%) | Memory: 44.6 MB (78.84%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -7,7 +7,7 @@ class Solution {
     }
     public void duplicateZeros(int[] arr) {
         int n = arr.length;
-        for(int i = 0; i < n - 1; i++) {
+        for(int i = 0; i < n; i++) {
             if(arr[i] == 0) {
                 shiftRight(arr, i);
                 i++;
