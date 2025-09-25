d2c1fe0b71fe71181fbbfc843fdf88218ad247ac
Rajnish Pandey <62322903+rajnish-pandey@users.noreply.github.com>
2025-09-25 09:07:53 +0530

    Time: 2 ms (71.31%) | Memory: 55.6 MB (41.16%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,11 +4,33 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i = 0; i<nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            if(nums[i] + nums[j] == target) {
-                return [i, j];
-            }
+    // Brute force 
+    // for(let i = 0; i<nums.length; i++) {
+    //     for(let j = i + 1; j < nums.length; j++) {
+    //         if(nums[i] + nums[j] == target) {
+    //             return [i, j];
+    //         }
+    //     }
+    // }
+
+    // using map
+    // let numMap = new Map();
+    // for(let i = 0; i < nums.length; i++) {
+    //     let complement  = target - nums[i];
+    //     if(numMap.has(complement)) {
+    //         return [numMap.get(complement), i];
+    //     }
+    //     numMap.set(nums[i], i);
+    // }
+
+    // using object
+    let numMap = {};
+    for(let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if(numMap[complement] !== undefined) {
+            return [numMap[complement], i]
         }
+        numMap[nums[i]] = i
     }
+    return []
 };
