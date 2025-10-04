da8acec6f85e5396e672f119a9d1a2298829c98c
Rajnish Pandey <62322903+rajnish-pandey@users.noreply.github.com>
2025-10-04 19:34:55 +0530

    Update number-of-good-pairs.java - Optimized Approach

--- 1635-number-of-good-pairs/number-of-good-pairs.java
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -1,13 +1,23 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         int ans = 0;
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < nums.length; j++) {
-                if((i < j) && (nums[i] == nums[j])) {
-                    ans += 1;
-                }
-            }
+        
+        // Brute Force Approach
+        // for(int i = 0; i < nums.length; i++) {
+        //     for(int j = 0; j < nums.length; j++) {
+        //         if((i < j) && (nums[i] == nums[j])) {
+        //             ans += 1;
+        //         }
+        //     }
+        // }
+
+        // using HashMap
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for(int num: nums) {
+            ans += freq.getOrDefault(num, 0);
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
+        
         return ans;
     }
 }
